3c1943d04298e5f6f1657a6d23cf9d0640d739f9
alger116 <viljarpartel@gmail.com>
2025-02-05 08:45:27 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+# GitHub Pages gem
+gem 'github-pages', group: :jekyll_plugins
+gem "webrick" # Required for Jekyll in Ruby 3+
---
+++ _site/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E3A8A',
+        secondary: '#10B981',
+        accent: '#F59E0B',
+        danger: '#EF4444',
+        warning: '#FBBF24',
+        info: '#3B82F6',
+        light: '#F3F4F6',
+        dark: '#111827',
+        buttonRed: '#FF0000',
+        buttonGreen: '#00FF00',
+        buttonBlue: '#0000FF',
+        buttonYellow: '#FFFF00',
+        buttonPurple: '#800080',
+      },
+    },
+  },
+  plugins: [],
+}
+  
