62ce46aaaf1d4455a7ac6e8af294384fa076eaf5
alger116 <viljarpartel@gmail.com>
2025-02-10 21:27:36 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,9 +5,9 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth
 // Your Firebase Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBnjmvUZ8T8rjd69YlvrwpMFwpAkONWm4E",
-    authDomain: "riigihankeproject.firebaseapp.com",
-    projectId: "riigihankeproject",
-    storageBucket: "riigihankeproject.firebasestorage.app",
+    authDomain: "Riigihankeproject.firebaseapp.com",
+    projectId: "Riigihankeproject",
+    storageBucket: "Riigihankeproject.firebasestorage.app",
     messagingSenderId: "649897121077",
     appId: "1:649897121077:web:2ff628090dc51e4df8287c",
     measurementId: "G-CGK6YHSDQ4"
