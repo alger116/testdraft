660ca2489d75abfe79d691690f0727734121b0fb
alger116 <viljarpartel@gmail.com>
2025-02-24 23:23:30 +0200

    1

---
+++ config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCyT8XLIwrmsYgMYalK9I_lp6OfQucBLR0",
+  authDomain: "kalkulaator-9f3d1.firebaseapp.com",
+  projectId: "kalkulaator-9f3d1",
+  storageBucket: "kalkulaator-9f3d1.firebasestorage.app",
+  messagingSenderId: "127234313724",
+  appId: "1:127234313724:web:1f333fb8f3ff2995bcb679",
+  measurementId: "G-91VSY43W50",
+};
